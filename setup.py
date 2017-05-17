fa012eb34b1efa92dae50c05391a3e423cd93d10
Jack Neely <jjneely@42lines.net>
2017-05-17 16:39:06 -0400

    Cut 0.2.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@
 
 setup_args = {
     "name": "whisper-backup",
-    "version": "0.1.3",
+    "version": "0.2.0",
     "platforms": ["any"],
     "description": "Backup whisper DB files into S3 or Swift",
     "long_description": """\
